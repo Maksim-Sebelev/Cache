eac3be8575cc10a7f2c95a4374ba0e9917a7c0e5
Maksim-Sebelev <7905111@list.ru>
2025-09-10 19:49:16 +0300

    ZoV

--- Src/src/cache_test.cpp
+++ Src/src/cache_test.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "arc_cache.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    
-    std::cout << "govno" << std::endl;
+    std::cout << argv[1] << std::endl;
 
     return 0;
 }
